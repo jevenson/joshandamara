226ffa74d910645bb3f17191376195511fda11c5
Josh Evenson <jevenson@outlook.com>
2017-01-02 17:02:16 -0600

    mobile detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,39 @@ import { Component, AfterViewInit } from '@angular/core';
 export class AppComponent implements AfterViewInit{
     title = 'app works!';
 
+    isMobile: boolean = false;
+
     constructor() {
     }
 
     public ngAfterViewInit(): void {
+      var isMobile = {
+          Android: function() {
+              return navigator.userAgent.match(/Android/i);
+          },
+          BlackBerry: function() {
+              return navigator.userAgent.match(/BlackBerry/i);
+          },
+          iOS: function() {
+              return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+          },
+          Opera: function() {
+              return navigator.userAgent.match(/Opera Mini/i);
+          },
+          Windows: function() {
+              return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+          },
+          any: function() {
+              return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+          }
+      };
+
+      if (isMobile.any()) {
+        $('.cd-fixed-bg').addClass('scroll');
+      }
+      else {
+        $('.cd-fixed-bg').addClass('fixed');
+      }
+
     }
 }
