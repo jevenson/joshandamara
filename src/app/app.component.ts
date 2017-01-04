8f9b49abae6c0e30937c3e30cd16598389f5ad0b
Josh Evenson <jevenson@outlook.com>
2017-01-03 22:04:25 -0600

    fix for ie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,44 +11,8 @@ export class AppComponent {
     isMobile: boolean = false;
 
     constructor() {
+      if (navigator.userAgent.match(/Edge/i)) {
+        console.log("edge sucks m8");
+      }
     }
-
-    // public ngAfterViewInit(): void {
-    //     var isMobile = {
-    //         Android: function() {
-    //             return navigator.userAgent.match(/Android/i);
-    //         },
-    //         BlackBerry: function() {
-    //             return navigator.userAgent.match(/BlackBerry/i);
-    //         },
-    //         iOS: function() {
-    //             return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    //         },
-    //         Opera: function() {
-    //             return navigator.userAgent.match(/Opera Mini/i);
-    //         },
-    //         Windows: function() {
-    //             return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-    //         },
-    //         any: function() {
-    //             return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    //         }
-    //     };
-
-    //     if (isMobile.any()) {
-    //         $('.cd-fixed-bg').addClass('scroll');
-    //     }
-    //     else {
-    //         $('.cd-fixed-bg').addClass('fixed');
-    //     }
-
-    //     if (isMobile.Android) {
-    //         var bg = jQuery(".cd-fixed-bg");
-    //         // jQuery(window).resize(
-    //         // function resizeBackground() {
-    //         //     bg.height(jQuery(window).height() + 60);
-    //         // })
-    //         bg.height(jQuery(window).height() + 56);
-    //     }
-    // }
 }
