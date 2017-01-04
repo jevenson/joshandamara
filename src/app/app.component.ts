82b9b60378475d1325e4b356f4489e73321c8ed9
Josh Evenson <jevenson@outlook.com>
2017-01-03 20:26:52 -0600

    wew lad i think it works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component, AfterViewInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent {
     title = 'app works!';
 
     isMobile: boolean = false;
@@ -13,42 +13,42 @@ export class AppComponent implements AfterViewInit{
     constructor() {
     }
 
-    public ngAfterViewInit(): void {
-        var isMobile = {
-            Android: function() {
-                return navigator.userAgent.match(/Android/i);
-            },
-            BlackBerry: function() {
-                return navigator.userAgent.match(/BlackBerry/i);
-            },
-            iOS: function() {
-                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-            },
-            Opera: function() {
-                return navigator.userAgent.match(/Opera Mini/i);
-            },
-            Windows: function() {
-                return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-            },
-            any: function() {
-                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-            }
-        };
+    // public ngAfterViewInit(): void {
+    //     var isMobile = {
+    //         Android: function() {
+    //             return navigator.userAgent.match(/Android/i);
+    //         },
+    //         BlackBerry: function() {
+    //             return navigator.userAgent.match(/BlackBerry/i);
+    //         },
+    //         iOS: function() {
+    //             return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    //         },
+    //         Opera: function() {
+    //             return navigator.userAgent.match(/Opera Mini/i);
+    //         },
+    //         Windows: function() {
+    //             return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    //         },
+    //         any: function() {
+    //             return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    //         }
+    //     };
 
-        if (isMobile.any()) {
-            $('.cd-fixed-bg').addClass('scroll');
-        }
-        else {
-            $('.cd-fixed-bg').addClass('fixed');
-        }
+    //     if (isMobile.any()) {
+    //         $('.cd-fixed-bg').addClass('scroll');
+    //     }
+    //     else {
+    //         $('.cd-fixed-bg').addClass('fixed');
+    //     }
 
-        if (isMobile.Android) {
-            var bg = jQuery(".cd-fixed-bg");
-            // jQuery(window).resize(
-            // function resizeBackground() {
-            //     bg.height(jQuery(window).height() + 60);
-            // })
-            bg.height(jQuery(window).height() + 56);
-        }
-    }
+    //     if (isMobile.Android) {
+    //         var bg = jQuery(".cd-fixed-bg");
+    //         // jQuery(window).resize(
+    //         // function resizeBackground() {
+    //         //     bg.height(jQuery(window).height() + 60);
+    //         // })
+    //         bg.height(jQuery(window).height() + 56);
+    //     }
+    // }
 }
