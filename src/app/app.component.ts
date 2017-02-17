c1b534b8a2f5bbb2803a1e31ea702a767e26057e
Josh Evenson <jevenson@outlook.com>
2017-02-16 21:15:02 -0600

    actually use ios... whoops

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var window: any;
+
 @Component({
     selector: 'app-root',
     template: `
@@ -16,6 +18,7 @@ export class AppComponent {
     }
 
     public get iOS(): boolean {
-        return true;// /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+        // Have to test MSStream because iPhone may appear in the IE User Agent
+        return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
     }
 }
